a3a6e830e2b105c21a62e69964efe433192ce34f
Yuliia Hrabovenko <yuliia.hrabovenko@gmail.com>
2023-10-24 10:11:40 +0000

    GZ-2: Auth by OAuth2 and JWT

---
+++ src/main/java/net/guzari/auth/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package net.guzari.auth.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login()
+                .failureUrl("/login?error=true")
+                .successHandler(oAuth2SuccessHandler);
+        return http.build();
+    }
+}
